a33e4d98ce0cb698b54967ee11f2260c8002de16
jimmystones <jim.a.gregory@gmail.com>
2022-03-22 22:08:10 +0000

    Update framework

--- Arcade-MoonPatrol.sv
+++ Arcade-MoonPatrol.sv
@@ -29,7 +29,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [45:0] HPS_BUS,
+	inout  [48:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        CLK_VIDEO,
@@ -173,20 +173,25 @@ module emu
 	input         OSD_STATUS
 );
 
+///////// Default values for ports not used in this core /////////
+
+assign ADC_BUS  = 'Z;
+assign USER_OUT = '1;
+assign {UART_RTS, UART_TXD, UART_DTR} = 0;
+assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
 assign {SDRAM_DQ, SDRAM_A, SDRAM_BA, SDRAM_CLK, SDRAM_CKE, SDRAM_DQML, SDRAM_DQMH, SDRAM_nWE, SDRAM_nCAS, SDRAM_nRAS, SDRAM_nCS} = 'Z;
 assign {DDRAM_CLK, DDRAM_BURSTCNT, DDRAM_ADDR, DDRAM_DIN, DDRAM_BE, DDRAM_RD, DDRAM_WE} = '0;  
-assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
-assign {UART_RTS, UART_TXD, UART_DTR} = 0;
 
-assign VGA_F1    = 0;
-assign VGA_SCALER= 0;
-assign USER_OUT  = '1;
-assign LED_USER  = ioctl_download;
-assign LED_DISK  = 0;
-assign LED_POWER = 0;
-assign BUTTONS   = 0;
-assign AUDIO_MIX = 0;
+assign VGA_F1 = 0;
+assign VGA_SCALER = 0;
 assign HDMI_FREEZE = 0;
+
+assign AUDIO_MIX = 0;
+
+assign LED_USER  = ioctl_download;
+assign LED_DISK = 0;
+assign LED_POWER = 0;
+assign BUTTONS = 0;
 
 reg [1:0] ar;
 
